3d2081c78b83c2c4090eb350ef56c54cf672ffa1
Prudhvi <prudhvisai3772@gmail.com>
2026-06-23 16:40:26 +0530

    cache the encoder

--- RAG/retriever.py
+++ RAG/retriever.py
@@ -13,29 +13,43 @@ from langchain_classic.retrievers.document_compressors import (
 
 from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from config_vectorStore import get_embedding_model
+from functools import lru_cache
 
-embedding_model = get_embedding_model()
 
-redundancy_filter = EmbeddingsRedundantFilter(embeddings=embedding_model)
-reorderer = LongContextReorder()
-cross_encoder = CrossEncoderReranker(
-    model=HuggingFaceCrossEncoder(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2"),
-    top_n=10,
-)
+@lru_cache
+def get_encoder_model():
+    return HuggingFaceCrossEncoder(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2")
 
-retriever = get_vectorStore().as_retriever(
-    search_type="mmr",
-    search_kwargs={"k": 40, "lambda_mult": 0.25, "filter": {"doc_id": "story1"}},
-)
 
-pipeline = DocumentCompressorPipeline(
-    transformers=[redundancy_filter, cross_encoder, reorderer]
-)
+def get_encoder():
+    return CrossEncoderReranker(
+        model=get_encoder_model(),
+        top_n=10,
+    )
 
-compression_retriever = ContextualCompressionRetriever(
-    base_retriever=retriever, base_compressor=pipeline
-)
 
-result = compression_retriever.invoke("What is the moral of the story")
+@lru_cache
+def get_pipeline():
 
-print(result[0])
+    redundancy_filter = EmbeddingsRedundantFilter(embeddings=get_embedding_model())
+    reorderer = LongContextReorder()
+
+    pipeline = DocumentCompressorPipeline(
+        transformers=[redundancy_filter, get_encoder(), reorderer]
+    )
+
+    return pipeline
+
+
+def get_retriever(doc_id: str):
+
+    retriever = get_vectorStore().as_retriever(
+        search_type="mmr",
+        search_kwargs={"k": 40, "lambda_mult": 0.25, "filter": {"doc_id": doc_id}},
+    )
+
+    compression_retriever = ContextualCompressionRetriever(
+        base_retriever=retriever, base_compressor=get_pipeline()
+    )
+
+    return compression_retriever
