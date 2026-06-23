6defe4efcce39d42d0f43aab81a71cc8eb29393d
Prudhvi <prudhvisai3772@gmail.com>
2026-06-23 16:02:19 +0530

    add retriever

--- RAG/retriever.py
+++ RAG/retriever.py
@@ -1,9 +1,40 @@
 from config_vectorStore import get_vectorStore
+from langchain_classic.retrievers.contextual_compression import (
+    ContextualCompressionRetriever,
+)
+from langchain_community.document_transformers import (
+    EmbeddingsRedundantFilter,
+    LongContextReorder,
+)
+from langchain_classic.retrievers.document_compressors import (
+    DocumentCompressorPipeline,
+    CrossEncoderReranker,
+)
 
-vector_store = get_vectorStore()
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+from config_vectorStore import get_embedding_model
 
-retriever = vector_store.as_retriever()
+embedding_model = get_embedding_model()
 
-result = retriever.invoke("summarize the story")
+redundancy_filter = EmbeddingsRedundantFilter(embeddings=embedding_model)
+reorderer = LongContextReorder()
+cross_encoder = CrossEncoderReranker(
+    model=HuggingFaceCrossEncoder(model_name="cross-encoder/ms-marco-MiniLM-L-6-v2")
+)
 
-print(result)
+retriever = get_vectorStore().as_retriever(
+    searchType="mmr",
+    search_kwargs={"k": 10, "lambda_mult": 0.25, "filter": {"doc_id": "story1"}},
+)
+
+pipeline = DocumentCompressorPipeline(
+    transformers=[redundancy_filter, cross_encoder, reorderer]
+)
+
+compression_retriever = ContextualCompressionRetriever(
+    base_retriever=retriever, base_compressor=pipeline
+)
+
+result = compression_retriever.invoke("What is the moral of the story")
+
+print(result[0])
